a0b5d7cfd5fc12bc44fb6efd51c6be28e3aef25b
Jimmy Edberg <oathmountain@hotmail.com>
2018-06-25 17:58:35 +0200

    Cleaned up main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import com.googlecode.lanterna.input.Key;
-import com.googlecode.lanterna.input.KeyMappingProfile;
 import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.TerminalSize;
 
 public class Main {
     public static void main(String[] arg) {
