751449a3c5477bb823ac6b3404e635f872d2bf1b
Jimmy Edberg <oathmountain@hotmail.com>
2018-06-25 17:01:29 +0200

    Added intelliJ project and Lanterna

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import com.googlecode.lanterna.input.Key;
+import com.googlecode.lanterna.input.KeyMappingProfile;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.TerminalSize;
+
+public class Main {
+    Terminal terminal;// = new Terminal() {
+}
