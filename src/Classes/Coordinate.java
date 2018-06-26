72f9ad0b2231229fdabf03365128a11fc921414d
Johan <aronsson.86@gmail.com>
2018-06-26 10:10:33 +0200

    added setter to coordinate.java

--- src/Classes/Coordinate.java
+++ src/Classes/Coordinate.java
@@ -13,9 +13,17 @@ public class Coordinate {
         return y;
     }
 
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
     public Coordinate(int x, int y) {
         this.x = x;
         this.y = y;
     }
-    
+
 }
