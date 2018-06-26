a26bd8b8fc9e41c6d75794d7d3ae548de34c1b2b
Jimmy Edberg <oathmountain@hotmail.com>
2018-06-26 10:12:02 +0200

    Added structure to gameengine, gamelogic and renderer

--- src/Classes/GameEngine.java
+++ src/Classes/GameEngine.java
@@ -1,7 +1,26 @@
 package Classes;
 
 public class GameEngine {
+    private Renderer renderer;
+    private GameLogic gameLogic;
+    //private Drawable[] drawables;
+    private Map map;
+    private Crow crow;
+    public GameEngine(){
+        renderer = new Renderer();
+        initializeDrawables();
+    }
+
+    private void initializeDrawables() {
+        gameLogic = new GameLogic();
+        crow = new Crow();
+        map = new Map();
+    }
+
     public boolean tick() {
+        gameLogic.tick();
+        renderer.renderCrow(crow);
+        renderer.renderMap(map);
         return true;
     }
 }
--- src/Classes/Renderer.java
+++ src/Classes/Renderer.java
@@ -1,4 +1,30 @@
 package Classes;
 
+import com.googlecode.lanterna.TerminalFacade;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.nio.charset.Charset;
+
 public class Renderer {
+    private Terminal terminal;
+    //public static Coordinate windowMax;
+
+    public Renderer() {
+        terminal = TerminalFacade.createTerminal(System.in, System.out, Charset.forName("UTF8"));
+        terminal.enterPrivateMode();
+        terminal.setCursorVisible(false);
+       // windowMax = new Coordinate(terminal.getTerminalSize().getColumns(), terminal.getTerminalSize().getRows());
+    }
+
+    public void renderCrow(Crow crow) {
+        terminal.clearScreen();
+        /*for (Drawable g : objects) {
+            terminal.applyBackgroundColor(0);
+            //terminal.moveCursor(g.getX(), g.getY());
+            //terminal.putCharacter(g.getSymbol());
+        }*/
+    }
+    public void renderMap(Map map){
+
+    }
 }
