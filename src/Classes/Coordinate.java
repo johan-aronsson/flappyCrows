ef352623a406256bce5bfa397752fab8481a7962
Johan <aronsson.86@gmail.com>
2018-06-26 10:07:21 +0200

    Coordinate class added intX och intY

--- src/Classes/Coordinate.java
+++ src/Classes/Coordinate.java
@@ -1,4 +1,21 @@
 package Classes;
 
 public class Coordinate {
+
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
 }
