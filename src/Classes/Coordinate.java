9523ecf89335f6219b2fb8f6c1f39ca141e3c6b0
Jimmy Edberg <oathmountain@hotmail.com>
2018-06-26 10:19:05 +0200

    Added move for coordinate

--- src/Classes/Coordinate.java
+++ src/Classes/Coordinate.java
@@ -5,10 +5,18 @@ public class Coordinate {
     private int x;
     private int y;
 
+    public Coordinate(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public void move(int x, int y){
+        this.x += x;
+        this.y += y;
+    }
+
     public int getX() {
         return x;
     }
-
     public int getY() {
         return y;
     }
@@ -16,14 +24,7 @@ public class Coordinate {
     public void setX(int x) {
         this.x = x;
     }
-
     public void setY(int y) {
         this.y = y;
     }
-
-    public Coordinate(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
 }
