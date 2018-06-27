f7099e8a9b0c206000cfb43d47aef42de43d65ca
Jimmy Edberg <oathmountain@hotmail.com>
2018-06-27 13:30:36 +0200

    Cleanupcrew

--- src/Classes/Coordinate.java
+++ src/Classes/Coordinate.java
@@ -1,13 +1,10 @@
 package Classes;
 
-import com.googlecode.lanterna.terminal.TerminalSize;
-
 public class Coordinate {
     private int x;
     private int y;
     private int color;
-
-
+    
     public Coordinate(int x, int y, int color) {
         setX(x);
         setY(y);
